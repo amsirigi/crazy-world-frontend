e4977ee561e663e4374c71dbe6e5bbe58e852e41
Pratiksha1798 <pratiksha.jamaiwar@capgemini.com>
2023-12-19 09:31:53 +0530

    Added the login , login display and patch components
    Added login display
    Added component

---
+++ src/app/components/logindisplay/logindisplay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logindisplay',
+  templateUrl: './logindisplay.component.html',
+  styleUrl: './logindisplay.component.css'
+})
+export class LogindisplayComponent {
+  countrylanguagefact:string = "Welcome to the Administrator Dashboard! This centralized hub empowers you to efficiently manage and update vital information within our system. From overseeing demographic shifts to refining critical data points, this platform is your gateway to ensuring accuracy and consistency across the board. Explore the diverse range of update sections, utilize intuitive tools, and stay informed with the latest system enhancements. Your pivotal role in maintaining the integrity and relevance of our data is invaluable. Let's embark on this journey together to foster an ecosystem of precision and progressng ";
+}
